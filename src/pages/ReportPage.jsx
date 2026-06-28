752d23e59a7dd8c8bbded514c9a78699b383adcf
Sreelakshmi <sreelakshmiramesh296@gmail.com>
2026-06-28 23:13:25 +0530

    Fix Supabase export/import

--- src/pages/ReportPage.jsx
+++ src/pages/ReportPage.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { supabase } from "../lib/supabase";
+import supabase from "../lib/supabase";
 import { useEffect, useState } from "react";
 import Report from "../components/Report";
 
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,4 +1,4 @@
-import { supabase } from "../lib/supabase";
+import  supabase  from "../lib/supabase";
 
 export async function signUp(email, password) {
   const { data, error } = await supabase.auth.signInWithPassword({
